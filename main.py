5d47bdcefa6f12abf4d25049ec9ec6b33644f451
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-09 20:50:55 +0300

    Capture data from form and display
    in an independent html document

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login/")
+def login():
+    name = request.args.get('name', type=str)
+    password = request.args.get('password', type=str)
+    print(name, password)
+    return render_template("login.html", name=name, password=password)
 
 
 if __name__ == "__main__":
